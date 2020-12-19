d30c943d9cfe3995e60bb89972aaaad73782b7aa
ssssota <tommysouta@gmail.com>
2020-12-20 01:13:10 +0900

    remove extra import

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@
 // import './index.css';
 // import stores from './stores';
 // import reportWebVitals from "./reportWebVitals";
-import { LiffProvider } from 'react-liff';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
@@ -19,9 +18,7 @@ export const stubEnabled = process.env.NODE_ENV !== 'production';
 ReactDOM.render(
 
     <Provider store={store}>
-        <LiffProvider liffId={liffId} stubEnabled={stubEnabled}>
-            <App/>
-        </LiffProvider>
+        <App/>
     </Provider>
     , document.getElementById('root'));
 
