682c591a172da4cf7ad87d393bcde032aae6158f
ssssota <tommysouta@gmail.com>
2020-12-19 13:38:41 +0900

    add calendar component

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,7 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { zeroPadding } from '../utils';
+import './Calendar.css';
 
 export const Calendar: React.FC = () => {
   const [date, setDate] = useState(new Date());
+  
+  useEffect(() => {
+    setTimeout(() => setDate(new Date()), 3600);
+  });
 
-  return <></>;
+  return (
+    <div className="Calendar">
+      <p className="Calendar-months-wrapper">
+        <span className="Calendar-months">{zeroPadding(date.getMonth() + 1)}</span>
+      </p>
+      <p className="Calendar-days-wrapper">
+        <span className="Calendar-days">{zeroPadding(date.getDate())}</span>
+      </p>
+    </div>
+  );
 }
